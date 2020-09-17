e39a1dd45756c36739f8577afade51a10614bbed
Aiquinones <aiquinones@uc.cl>
2020-09-17 00:44:42 -0300

    CLEAN: remove extra \ufeff chars

---
+++ detect_errors.py
@@ -0,0 +1,17 @@
+def search_lines_for_errors(filename):
+  print(f'Looking for errors in {filename}')
+  errors = 0
+  with open(filename, 'r') as f:
+    for i, line in enumerate(f.readlines()):
+      if '\ufeff' in line:
+        print(f'Error in line {i+1}')
+        errors += 1
+  if errors == 0:
+    print('No errors found!')
+  else:
+    print(f'{errors} errors were found')
+
+for partition in ['dev', 'test', 'train']:
+  filename = f'es_gsd-ud-{partition}.conllu'
+  search_lines_for_errors(filename)
+  print('='*20)
